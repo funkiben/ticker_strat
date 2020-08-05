01d7ae9ef5b67ca50b289a0a631c726c215ff107
funkiben <funkiben@gmail.com>
2020-08-04 20:42:08 -0400

    Use new From impls for Response.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ use std::io::Error;
 use std::sync::Arc;
 use std::time::Duration;
 
-use my_http::common::header::CONTENT_LENGTH;
-use my_http::common::response::Response;
-use my_http::common::status;
-use my_http::header_map;
 use my_http::server::{Config, Server};
 use my_http::server::ListenerResult::SendResponseArc;
 
@@ -17,16 +13,9 @@ fn main() -> Result<(), Error> {
         tls_config: None,
     });
 
-    let response_message = b"I work!";
-    let response = Response {
-        status: status::OK,
-        headers: header_map![
-            (CONTENT_LENGTH, response_message.len().to_string())
-        ],
-        body: response_message.to_vec(),
-    };
-
+    let response = "I work!".into();
     let response = Arc::new(response);
+
     server.router.on_prefix("/", move |_, _| {
         println!("Received a request");
         SendResponseArc(Arc::clone(&response))
