02c57bff829d50bd5df4250bd1fd0a5cf92f9e0b
funkiben <funkiben@gmail.com>
2020-08-16 22:02:50 -0400

    Cache uses RwLock instead of Mutex Updated to support more content-types

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+use std::collections::HashMap;
+use std::fs;
 use std::io::Error;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::time::Duration;
 
-use my_http::server::{Config, Server, Router};
-use my_http::server::ListenerResult::SendResponseArc;
-use std::collections::HashMap;
+use my_http::common::header;
 use my_http::common::response::Response;
 use my_http::common::status;
-use my_http::common::header;
 use my_http::header_map;
-use std::fs;
+use my_http::server::{Config, Router, Server};
+use my_http::server::ListenerResult::SendResponseArc;
 
 fn main() -> Result<(), Error> {
     let mut server = Server::new(Config {
         addr: "0.0.0.0:80",
-        connection_handler_threads: 10,
+        connection_handler_threads: 5,
         read_timeout: Duration::from_millis(1000),
         tls_config: None,
     });
@@ -27,7 +27,7 @@ fn main() -> Result<(), Error> {
 fn file_router(directory: &'static str) -> Router {
     let mut router = Router::new();
 
-    let cache: Mutex<HashMap<String, Arc<Response>>> = Mutex::new(HashMap::new());
+    let cache: RwLock<HashMap<String, Arc<Response>>> = RwLock::new(HashMap::new());
 
     router.on_prefix("", move |uri, _| {
         let mut path = String::from(directory);
@@ -37,11 +37,16 @@ fn file_router(directory: &'static str) -> Router {
             path.push_str("index.html")
         }
 
-        let mut cache = cache.lock().unwrap();
 
-        let response = cache.entry(path.clone()).or_insert_with(|| Arc::new(file_response(&path)));
+        if let Some(response) = cache.read().unwrap().get(&path) { // read lock gets dropped after if statement
+            return SendResponseArc(Arc::clone(response));
+        }
 
-        SendResponseArc(Arc::clone(&response))
+        let response = Arc::new(file_response(&path));
+
+        cache.write().unwrap().insert(path, Arc::clone(&response));
+
+        SendResponseArc(response)
     });
 
     router
@@ -70,6 +75,10 @@ fn get_content_type(path: &str) -> &'static str {
         return "text/html";
     } else if path.ends_with(".css") {
         return "text/css";
+    } else if path.ends_with(".png") {
+        return "image/png";
+    } else if path.ends_with(".jpg") {
+        return "image/jpeg";
     }
     "text/plain"
 }
