a77daaa890c5d6e48ffc72a33f35381cb22744b1
JamesTann <jt.21@dartmouth.edu>
2020-12-05 16:30:31 -0500

    Basic graph working

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,40 +1,55 @@
 use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Eq, PartialEq)]
-struct Vertex<'a, T: Eq + PartialEq> {
-    id: usize,
+struct Vertex<T: Eq + PartialEq> {
+    label: usize,
     value: T,
-    neighbors: Vec<&'a RefCell<Vertex<'a, T>>>,
+    neighbors: Vec<Rc<RefCell<Vertex<T>>>>,
 }
 
-impl<'a, T: Eq + PartialEq + Clone> Vertex<'a, T> {
-    pub fn new(id: usize, value: T) -> RefCell<Vertex<'a, T>> {
-        RefCell::new(Vertex {
-            id,
+impl<T: Eq + PartialEq> Vertex<T> {
+    pub fn new(label: usize, value: T) -> Rc<RefCell<Vertex<T>>> {
+        Rc::new(RefCell::new(Vertex {
+            label,
             value,
             neighbors: Vec::new(),
-        })
+        }))
     }
 
-    pub fn add_neighbor(&mut self, neighbor: &'a RefCell<Vertex<'a, T>>) -> bool {
-        if !self.has_neighbor(neighbor) {
+    pub fn add_neighbor(&mut self, neighbor: Rc<RefCell<Vertex<T>>>) -> bool {
+        if !self.has_neighbor(&neighbor) {
             self.neighbors.push(neighbor);
             return true;
         }
         false
     }
 
-    pub fn has_neighbor(&self, neighbor: &RefCell<Vertex<'a, T>>) -> bool {
-        self.neighbors.contains(&neighbor)
+    pub fn has_neighbor(&self, neighbor: &Rc<RefCell<Vertex<T>>>) -> bool {
+        self.neighbors.contains(neighbor)
     }
 
     pub fn num_neighbors(&self) -> usize {
         self.neighbors.len()
     }
 
-    pub fn traverse(&self) {
-        let mut seen: Vec<usize> = Vec::new();
-        seen.push(self.id);
-    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic_graph_setup() {
+        let root = Vertex::new(0, "Hello");
+        let neighbor = Vertex::new(1, "World");
+
+        root.borrow_mut().add_neighbor(neighbor.clone());
+
+        let neighbors = root.borrow().num_neighbors();
+        assert_eq!(1, neighbors);
+
+        let has_neighbor = root.borrow().has_neighbor(&neighbor);
+        assert!(has_neighbor);
+    }
+}
